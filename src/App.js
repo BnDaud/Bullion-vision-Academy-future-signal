8531eab595fddb891048de1541cbd23dfa7b06d3
Bndaud <Lawalsulaimon70@gmail.com>
2024-06-26 06:18:08 +0100

    created new page, including logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
+import Displayanalysis from "./pages/displayAnalysis";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Displayanalysis />
     </div>
   );
 }
---
+++ src/pages/displayAnalysis.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import bva from "../image/bva.jpeg";
+
+export default function Displayanalysis() {
+  function getdate() {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const date = today.getDate();
+
+    return `${date} : ${month} : ${year}`;
+  }
+
+  const [date, setDate] = useState(getdate());
+  return (
+    <>
+      <div className="container mx-auto p-4 bg-[#fcfcfc] h-screen">
+        <img src={bva} alt="BVA_Logo" className="w-22 h-10" />
+        <div className="grid grid-cols-12 gap-4 mt-20 ml-20">
+          <div className="bg-green-200 col-span-8"> hello</div>
+          <div className="bg-red-200 col-span-3">
+            <div className=" border border-1 border-black rounded-md p-1 pl-2 mb-3 text-center">
+              {date}
+            </div>
+            <div className="mt-5 border border-1 border-black rounded-md p-1">
+              {" "}
+              hello
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
